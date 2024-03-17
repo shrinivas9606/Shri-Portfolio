c3a041e8196372d21ec191b60be6965a67bebaa8
shrinivas9606 <shrinivas.narhare.cs@ghrcem.raisoni.net>
2024-03-17 15:35:16 +0530

    Added images & Copyright

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Gautam Patil.
+      <time>{YEAR}</time> © Shrinivas Narhare.
       <style jsx>{`
         a {
           float: right;
